a48e829afef29d8fd62357ef29afc750ce8306fa
Wellington Lopes <w.camara.lopes@gmail.com>
2018-08-02 11:50:57 -0300

    Serialize de objetos e arrays no form

--- js/Form.js
+++ js/Form.js
@@ -2,24 +2,89 @@
 
 class Form {
 
+	static get RPCversion () {
+		return "2.0";
+	}
+
 	static get url () {
-		let url       = window.location.href.replace(/#.*/, "").replace(/\/$/, "");
-		let bergamota = "/cgi-bin/berga-rpc";
-		return url + bergamota;
+		let url = window.location.href.replace(/#.*/, "").replace(/\/$/, "");
+		let cgi = "/cgi-bin/berga-rpc";
+		return url + cgi;
 	}
 
 	static submit (form) {
-		let dados  = this.serialize(form);
-		let method = form.getAttribute("action");
-		console.log(this.url, method, dados);
+		let send       = {};
+		let serialized = this.serialize(form);
+		let method     = form.getAttribute("action");
+		send.jsonrpc   = this.RPCversion;
+		send.method    = method;
+		send.params    = serialized;
+		send.id        = parseInt(Math.random() * 10000).toString();
+		let dados      = JSON.stringify(serialized);
+		let init       = {
+			credentials : "include",
+			method      : "POST",
+			body        : dados,
+			headers: {
+				"Accept"           : "application/json",
+				"Application-Type" : "text/json",
+				"Content-Length"   : dados.length,
+			}
+		};
+
+		window.fetch(this.url, init).then(response => {
+			if (!response.ok){
+				window.alert(_t("request_failed"));
+				return;
+			}
+			response.json().then(json => {
+				console.log(json);
+				window.alert(_t("request_success"));
+			}).catch(() => {
+				window.alert(_t("request_failed"));
+				return;
+			});
+		});
+
 		return false;
 	}
 
 	static serialize (form) {
 		let dados  = {};
 		let inputs = form.querySelectorAll("input, select");
+		let nest = (obj, key_path, value) => {
+			let last_key = key_path.length - 1;
+			for (let i=0; i<last_key; i++) {
+				let key = key_path[i];
+				if (!(key in obj)) {
+					obj[key] = {};
+				}
+				obj = obj[key];
+			}
+
+			if (typeof obj[key_path[last_key]] === "undefined") {
+				obj[key_path[last_key]] = value;
+				return;
+			}
+
+			if (!Array.isArray(obj[key_path[last_key]])){
+				obj[key_path[last_key]] = [obj[key_path[last_key]]];
+			}
+			obj[key_path[last_key]].push(value);
+		};
+
 		inputs.forEach(input => {
-			dados[input.id] = input.value;
+			let name = input.getAttribute("name")
+				.replace(/\]/, "")
+				.replace(/\[/, ".")
+				.split(".");
+
+			if (name.length === 1){
+				dados[name[0]] = input.value;
+				return;
+			}
+
+			nest(dados, name, input.value);
 		});
 		return dados;
 	}
--- js/lang/pt-br.js
+++ js/lang/pt-br.js
@@ -2,5 +2,7 @@ var pt_br = {
 	msg_footer: "\"Eu tenho um router que veio diretamente da Turquia. Foi ele que conectou os árabes!\"",
 	//msg_footer: "\"I got some tangerine routers straight from Turkey, boy. It's what gets those arabs online!\"",
 	wireless: "Wireless",
-	lanwan: "LAN/WAN"
+	lanwan: "LAN/WAN",
+	request_failed: "Falha no envio dos dados!",
+	request_success: "Dados salvos!"
 };
